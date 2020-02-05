68de825fd31c8a31b00bb516042293e260fc76e4
fangfangxu <472848022@qq.com>
2020-02-05 22:18:02 +0800

    commit
    commit

--- src/main/java/com/imooc/springmvc/controller/TestController.java
+++ src/main/java/com/imooc/springmvc/controller/TestController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * 测试Controller
+ */
 @Controller
 public class TestController {
     @GetMapping("/t")
