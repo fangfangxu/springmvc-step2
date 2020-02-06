6239c27c687d517e8b5ba1c30f9c283ddf867169
fangfangxu <472848022@qq.com>
2020-02-06 14:23:46 +0800

    commit
    commit

--- src/main/java/com/imooc/springmvc/controller/UrlMappingController.java
+++ src/main/java/com/imooc/springmvc/controller/UrlMappingController.java
@@ -2,19 +2,21 @@ package com.imooc.springmvc.controller;
 
 import com.imooc.springmvc.entity.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 @RequestMapping("/um")
 public class UrlMappingController {
 
-    @GetMapping("/get")
+    @PostMapping("/login")
     @ResponseBody
-    public String post(User user){
-        System.out.println(user.getUsername()+":"+user.getPassword());
-        return "This is a post method!";
+    public String login(User user) {
+        String username = user.getUsername();
+        String password = user.getPassword().toString();
+        return "<fieldset><legend>登录成功</legend>用户名:" + username + "<br>密码：" + password + "</fieldset>";
+
     }
 }
